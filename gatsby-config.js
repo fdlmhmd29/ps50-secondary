30ffb094f7efd0027d61e019f7fd04c5b687cd0c
fdlmhmd29 <fadhilmhmd29@gmail.com>
2022-01-15 02:37:58 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Max More Studio`,
@@ -54,7 +58,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-217181605-1",
+        trackingId: "UA-217181605-1" || process.env.GA_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
